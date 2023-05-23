fc3a721bdfe5b8e99696b8cc34e455d4b005db83
Ridwan Atolagbe <ibnsulaimaan@gmail.com>
2023-05-23 21:06:19 +0200

    Renewed Springboot learning Day 1

---
+++ src/test/java/com/abuuaasiyah/coolApp/CoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abuuaasiyah.coolApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
